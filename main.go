8b94fabd19a659c389b080c08b66d2ec4e15c63b
bibek6 <bibek@quikieapps.com>
2021-08-20 23:04:15 +0530

    started basics of type conversion and slices

--- main.go
+++ main.go
@@ -4,5 +4,17 @@ func main() {
 	// var card string = "Ace of spades"
 	cards := newDeck()
 
-	cards.print()
+	cards.saveTotFile(("my_cards"))
+
+	// fmt.Println(cards.toString())
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	// type conversion example
+	// greeting := "Hi there!"
+	// fmt.Println([]byte(greeting))
+	// Output: [72 105 32 116 104 101 114 101 33]
 }
